d1c0e19c50c0c5a257dc323dc3d0a24f6759bd84
Yunus <yunusteach@gmail.com>
2025-03-20 14:49:57 +0700

    update kopintar

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-// Toggle class active
+// Toggle class active untuk hamberger menu
 const navbarNav = document.querySelector(".navbar-nav");
+
 // hamburger menu diklik
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
-// klik diluar menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active')
-    }
+//toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
+};
+
+// klik diluar menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
 });
