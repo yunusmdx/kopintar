643a09c74a2b0c753b9b250593fc484e676982b1
Yunus <yunusteach@gmail.com>
2025-04-29 10:46:50 +0700

    up-kopintar

--- js/script.js
+++ js/script.js
@@ -10,15 +10,23 @@ document.querySelector("#hamburger-menu").onclick = () => {
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
-document.querySelector("#search-button").onclick = () => {
+document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
 };
 
+//klik diluar elemen
 // klik diluar menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
+const searchBtn = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
